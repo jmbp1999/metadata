2ff4a181ef7d42c0bb6ef1155545a35e287febd3
jmbp1999 <pamudithaj98@gmail.com>
2024-03-21 14:49:00 +0530

    1

---
+++ app/chat/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import {useRouter} from "next/navigation";
+
+export default function ChatPage() {
+  return (
+    <main className={styles.main}>
+   
+    </main>
+  );
+}
