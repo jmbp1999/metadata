d3883091ae0c2d0a3da8d7b8e5e97c926657161a
jmbp1999 <pamudithaj98@gmail.com>
2024-04-25 15:56:38 +0530

    test4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,8 @@
-import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  openGraph: {
-    title: 'Next.js',
-    description: 'The React Framework for the Web',
-    url: 'https://nextjs.org',
-    siteName: 'Next.js',
-    images: [
-      {
-        url: 'https://courses.cs.washington.edu/courses/cse154/19sp/staff/photos/conner-ardman.jpg',
-        width: 800,
-        height: 600,
-      },
-      {
-        url: 'https://courses.cs.washington.edu/courses/cse154/19sp/staff/photos/conner-ardman.jpg',
-        width: 1800,
-        height: 1600,
-        alt: 'My custom alt',
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-};
- 
 
 export default function RootLayout({
   children,
