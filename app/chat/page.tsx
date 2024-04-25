80ef94519248eb59f36c5da2f318cbaaaeb7ff8e
jmbp1999 <pamudithaj98@gmail.com>
2024-04-25 14:14:10 +0530

    21

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,29 +1,66 @@
 import { Metadata, ResolvingMetadata } from "next";
+import { ImageResponse } from "next/og";
 type Props = {
   params: { id: string };
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
-
+export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<ImageResponse> {
   const product = await fetch(`https://o7wc2ghhopwh7yac3an4sy6nma0ptyhu.lambda-url.us-east-1.on.aws/`).then((res) => res.json());
 
-  return {
-    title: product.liveVersion.name,
-    description: product.liveVersion.description,
-    openGraph: {
-      title: product.liveVersion.name,
-      description: product.liveVersion.description,
-      siteName: "ConvoGrid",
-      images: product.liveVersion.profileUrl,
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: product.liveVersion.name,
-      description: product.liveVersion.description,
-      images: product.liveVersion.profileUrl,
-    },
-  };
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          backgroundColor: "black",
+          backgroundSize: "150px 150px",
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            justifyItems: "center",
+          }}
+        >
+          <img
+            alt="Vercel"
+            height={200}
+            src="data:image/svg+xml,%3Csvg width='116' height='100' fill='white' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M57.5 0L115 100H0L57.5 0z' /%3E%3C/svg%3E"
+            style={{ margin: "0 30px" }}
+            width={232}
+          />
+        </div>
+        <div
+          style={{
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            color: "white",
+            marginTop: 30,
+            padding: "0 120px",
+            lineHeight: 1.4,
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          ConvoGrid
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
 }
 
 export default function ChatPage() {
