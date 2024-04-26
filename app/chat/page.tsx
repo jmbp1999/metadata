164f65b140872d2e220c14f4bdec75f58f04cc0b
jmbp1999 <pamudithaj98@gmail.com>
2024-04-26 15:13:03 +0530

    cdcdc

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,3 +1,18 @@
+
+export const metadata = {
+  title:"ConvoGrid",
+  description: 'Create Conversational AI Bots with ease',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ConvoGrid',
+    description: 'Create Conversational AI Bots with ease',
+  },
+  openGraph: {
+    title: 'ConvoGrid',
+    description: 'Create Conversational AI Bots with ease',
+  },
+}
+
 export default function ChatPage() {
   return (
     <div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 import styles from "./page.module.css";
 import Button from "@/components/button";
 
+export const metadata = {
+  title:"ConvoGrid",
+  description: 'Create Conversational AI Bots with ease',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ConvoGrid',
+    description: 'Create Conversational AI Bots with ease',
+  },
+  openGraph: {
+    title: 'ConvoGrid',
+    description: 'Create Conversational AI Bots with ease',
+  },
+}
+
 export default function Home() {
   return (
     <main className={styles.main}>
