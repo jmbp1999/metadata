56458e6b181491cce293bb964421410aa096c0e1
jmbp1999 <pamudithaj98@gmail.com>
2024-04-26 10:28:41 +0530

    test7

--- app/chat/opengraph-image.tsx
+++ app/chat/opengraph-image.tsx
@@ -1,23 +1,51 @@
-import { ImageResponse } from "@vercel/og";
-
-
+import { ImageResponse } from 'next/og'
+ 
+// Image metadata
+export const alt = 'About Acme'
 export const size = {
   width: 1200,
   height: 630,
-};
-
-const daysOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-export default function Image() {
-  const today = new Date();
-  const dayName = daysOfWeek[today.getDay()];
-  return new ImageResponse(<div style={{display:"flex"}}>Happy {dayName}!</div>, { ...size });
+}
+ 
+export const contentType = 'image/png'
+ 
+// Image generation
+export default async function Image() {
+  // Font
+  const interSemiBold = fetch(
+    new URL('./Inter-SemiBold.ttf', import.meta.url)
+  ).then((res) => res.arrayBuffer())
+ 
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        About Acme
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: 'Inter',
+          data: await interSemiBold,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
+    }
+  )
 }
